31f98d4ab7cae5f2ed21e1aa64d9fcb2eb700782
Robert Tyrees <robert.tyree@stinkstudios.com>
2019-07-09 11:54:39 +0100

    make fe better

--- frontend/src/components/app/index.js
+++ frontend/src/components/app/index.js
@@ -1,23 +1,11 @@
 import React from 'react';
+import bottlecap from './bottlecap.gif';
 import styles from './index.module.css';
-import logo from './logo.svg';
 
 const App = () => (
   <div className={styles.root}>
-    <header className={styles.header}>
-      <img src={logo} className={styles.logo} alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className={styles.link}
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
+  <h1>THANKS ALEX!</h1>
+    <img src={bottlecap} alt="win"/>
   </div>
 );
 
